8d8a81300fe2fe148ea0e0ec71d11baad55d2bda
Alireza <alirezasoran@gmail.com>
2018-10-23 15:26:16 +0330

    fibo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int x = 0, y = 1, sum=0, sum2=0;;
+    while(true){
+        sum=x+y;
+        if(sum>4000000){
+            break;
+        }
+        if(sum%2==0){
+            sum2=sum+sum2;
+        }
+        x=y;
+        y=sum;
+    }
+cout<<"sum is: "<<sum2;
+
+    cin.get();
 }
